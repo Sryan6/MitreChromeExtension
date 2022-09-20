3a8d19bbb16b457c34b5bfb4674fa53501f20858
Sryan6 <sryan6@umbc.edu>
2022-09-20 16:56:57 -0400

    This time, it will work with no server and returns active tab

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # MitreChromeExtension
 
 npm init -y
+npm i nodemon -g
+
+code originally grabbed from https://github.com/kubowania/nodejs-webscraper and modified
+
+npm run start to start
--- src/app.js
+++ src/app.js
@@ -9,16 +9,17 @@ chrome.action.onClicked.addListener((tab) => {
     }
 });
 
-fetch('http://localhost:8000/results')
-    .then(response => { return response.json() })
-    .then(data => {
-        data.forEach(article => {
-            const articleItem = `<div><h3>` + article.keyword + `</h3><p>` + article.tCode + `</p></div>`
-            feedDisplay.insertAdjacentHTML("beforeend", articleItem)
-        })
 
-    })
-    .catch(err => console.log(err))
+//fetch('http://localhost:8000/results')
+//    .then(response => { return response.json() })
+//    .then(data => {
+//        data.forEach(article => {
+//            const articleItem = `<div><h3>` + article.keyword + `</h3><p>` + article.tCode + `</p></div>`;
+//            feedDisplay.insertAdjacentHTML("beforeend", articleItem);
+//        })
+
+//    })
+//    .catch(err => console.log(err))
 
 
 
