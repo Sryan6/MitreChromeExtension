17e7f1acf7fd211a92b79e3a83f8efd24150fb33
Sryan6 <sryan6@umbc.edu>
2022-09-29 16:46:28 -0400

    Doesn't look pretty, but the copy button finally works.

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 //var app = chrome.runtime.getBackgroundPage();
+let textToCopy = ""
 
 async function hello() {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -32,12 +33,15 @@ async function hello() {
                         if (data[0].result.length == 0) {
                             feedDisplay.insertAdjacentHTML("afterbegin", "No T-Codes found");
                         }
+                        textToCopy = "";
                         for (let i = 0; i < data[0].result.length; i++) {
                             console.log("data.result");
                             console.log(data[0].result);
                             feedDisplay.insertAdjacentHTML("beforeend", data[0].result[i]);
                             feedDisplay.insertAdjacentHTML("beforeend", "<br />");
+                            textToCopy = textToCopy + data[0].result[i] + ", ";
                         }
+                        textToCopy = textToCopy.slice(0, -2);
                     }
                 }
                 //feedDisplay.insertAdjacentHTML("beforeend", data[0].result);
@@ -45,9 +49,89 @@ async function hello() {
 
         }
     });
+
+}
+
+async function copy() {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        //files: ['alert.js'],
+        func: copyText,
+    }, (data) => {
+        console.log("The returned data:");
+        console.log(data);
+        console.log("textToCopy: ");
+        console.log(textToCopy);
+        //Create a textbox field where we can insert text to. 
+        var copyFrom = document.createElement("textarea");
+
+        //Set the text content to be the text you wished to copy.
+        copyFrom.textContent = textToCopy;
+
+        //Append the textbox field into the body as a child. 
+        //"execCommand()" only works when there exists selected text, and the text is inside 
+        //document.body (meaning the text is part of a valid rendered HTML element).
+        document.body.appendChild(copyFrom);
+
+        //Select all the text!
+        copyFrom.select();
+
+        //Execute command
+        document.execCommand('copy');
+
+        //De-select the text using blur(). 
+        copyFrom.blur();
+
+        //Remove the textbox field from the document.body, so no other JavaScript nor 
+        //other elements can get access to this.
+        document.body.removeChild(copyFrom);
+    })
+}
+
+async function copyText() {
+    // get the text to be copied
+    elem = document.getElementById('texthere')
+    console.log(elem);
+    console.log("in function copyText");
+    if (elem.innerText != undefined) {
+        console.log("in 1");
+        text = elem.innerText;
+    }
+    else if (elem.value != undefined) {
+        console.log("in 2")
+        text = elem.value;
+    } else {
+        console.log("returned false")
+        //return false;
+    }
+
+    console.log("text to copy:");
+    console.log(text);
+
+    // create a temporary box, and stop the viewport scrolling
+    var box = document.createElement("textarea");
+    box.value = text;
+    document.body.appendChild(box);
+    box.style.top = "0";
+    box.style.left = "0";
+    box.style.position = "fixed";
+
+    // select the text in the box and copy it
+    box.focus();
+    box.select();
+    box.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+    // alert the user of the copy?
+    //alert("text copied to your clipboard!");
 }
 
 document.getElementById('clickme').addEventListener('click', hello);
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('copyButton').addEventListener('click', copy);
+});
+
 
 async function testFunction() {
     html = document.body.innerHTML;
@@ -107,11 +191,11 @@ async function testFunction() {
     // Search the string and counts the number of e's
     for (let j = 0; j < tCodeKeys.length; j++) {
         //for (let k = 0; k < sizeOfhtml; k++) {
-        while(pos != -1){
+        while (pos != -1) {
             pos = html.indexOf(tCodeKeys[j], i + 1);
             console.log(pos)
             html = html.slice(0, pos) + "<span style='background-color: #FFFF00'>" + html.slice(pos, pos + tCodeKeys[j].length) + "</span>" + html.slice(pos + tCodeKeys[j].length);
-            
+
             //k = k + tCodeKeys[j].length + 47;
             num += 1;
             i = pos;
@@ -120,6 +204,7 @@ async function testFunction() {
                 break;
             }
         }
+        console.log(html);
     }
     //console.log("newHTML");
     //console.log(newHTML);
@@ -133,6 +218,7 @@ async function testFunction() {
     console.log(num);
 
     //document.body.style.backgroundColor = 'purple';
-    //document.write("<p>Test</p><strong>TEST</strong>")
+    //document.wrs.ryan2@acclaimite("<p>Test</p><strong>TEST</strong>")
+    //document.write(html)
     return tCodeList;
 }
